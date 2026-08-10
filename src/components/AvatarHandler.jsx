4f5b865b179bef66580e2dfd94b4447b05d04fc6
s-d-august <151471356+s-d-august@users.noreply.github.com>
2026-08-10 11:25:36 -0500

    fixing images

--- src/components/AvatarHandler.jsx
+++ src/components/AvatarHandler.jsx
@@ -1,14 +1,30 @@
 import { Image } from "react-bootstrap"
+import breath1 from "../files/breath1.png"
+import breath2 from "../files/breath2.png"
+import breath3 from "../files/breath3.png"
+import def from "../files/def.png"
+import earnest from "../files/earnest.png"
+import excited from "../files/excited.png"
+import faceDefault from "../files/face.png"
+import gentle from "../files/gentle.png"
+import pleased from "../files/pleased.png"
+import worried from "../files/worried.png"
 
-const avatarImages = require.context("../files", false, /\.png$/)
+const avatarImages = {
+    breath1,
+    breath2,
+    breath3,
+    def,
+    earnest,
+    excited,
+    face: faceDefault,
+    gentle,
+    pleased,
+    worried,
+}
 
 const AvatarHandler = ({ face }) => {
-    console.log("avatar handler", face)
-
-    const imageKey = `./${face}.png`
-    const expression = avatarImages.keys().includes(imageKey)
-        ? avatarImages(imageKey)
-        : console.log("expression not found")
+    const expression = avatarImages[face] || avatarImages.def
 
     return (
         <Image id="avatarImg" src={expression} />
